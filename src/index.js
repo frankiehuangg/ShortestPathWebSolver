6a183680453f27bbd1fc475e97e77a33c60633cf
frankiehuangg <frankie.huangg@gmail.com>
2023-04-12 15:08:05 +0700

    feat: many functionalities

--- src/index.js
+++ src/index.js
@@ -38,19 +38,21 @@ function addMarker(coords) {
 
 // Delete marker function
 function deleteMarker() {
-	markers[0].setMap(null);
-	markers.splice(0, 1);
-	locations.splice(0, 1);
+	removePaths();
+	for (var i = 0; i < markers.length; i++)
+		markers[i].setMap(null);
+	markers = [];
+	locations = [];
 }
 
 // Add lines to map
-function drawPath(coords, stroke) {
+function drawPath(coords, stroke, weight) {
 	var path = new google.maps.Polyline({
 		path: coords,
 		geodesic: true,
 		strokeColor: stroke,
 		strokeOpacity: 1.0,
-		strokeWeight: 2,
+		strokeWeight: weight,
 	});
 
 	path.setMap(map);
@@ -76,14 +78,9 @@ function isValidNodeAmount() {
 
 // Main function algorithm
 function findShortestPath() {
-	if (modes == 0) {
-		alert("Please choose an algorithm first!");
-		return;
-	}
+	removePaths();
 
 	if (isValidNodeAmount()) {
-		removePaths();
-
 		var distanceMatrix = [];
 		for (var i = 0; i < locations.length; i++) {
 			distanceMatrix[i] = new Array(locations.length);
@@ -96,41 +93,104 @@ function findShortestPath() {
 		}
 
 		for (var i = 0; i < locations.length; i++) {
-			var shortest = [6371*2, 0, 0];
+			var shortest = [[6371, 0, 0]];
 
-			for (var j = i+1; j < locations.length; j++) {
+			for (var j = 0; j < locations.length; j++) {
+				if (i == j) 
+					continue;
+
 				var value = calculateDistance(locations[i], locations[j]);
 
-				if (value < shortest[0]) {
-					shortest = [value, i, j];
+				if (value < shortest[0][0]) {
+					shortest[0][0] = value;
+					shortest[0][1] = i;
+					shortest[0][2] = j;
 				}
 			}
-			
-			distanceMatrix[shortest[1]][shortest[2]] = shortest[0];
-			distanceMatrix[shortest[2]][shortest[1]] = shortest[0];
 
-			var coords = [locations[shortest[1]], locations[shortest[2]]];
-			console.log(coords);
-			drawPath(coords, "#000000")
+			for (var j = 0; j < locations.length; j++) {
+				if (i == j) 
+					continue;
+
+				var value = calculateDistance(locations[i], locations[j]);
+
+				if (value < 0.1 && distanceMatrix[i][j] == 0) {
+					shortest.push([value, i, j]);
+				}
+			}
+
+			for (var j = 0; j < shortest.length; j++) {
+				distanceMatrix[shortest[j][1]][shortest[j][2]] = shortest[j][0];
+				distanceMatrix[shortest[j][2]][shortest[j][1]] = shortest[j][0];
+
+				var coords = [locations[shortest[j][1]], locations[shortest[j][2]]];
+				drawPath(coords, "#000000", 1)
+			}
 		}
 
-		var steps = modes == 1 ? ucs(0, locations.length-1, distanceMatrix) : aStar(0, locations.length-1, distanceMatrix);
+		console.log(locations);
+
+		var startNode = parseInt(document.getElementById("start-node").value) - 1;
+		var finishNode = parseInt(document.getElementById("finish-node").value) - 1;
+
+		if (typeof(startNode) != "number" && typeof(finishNode) != "number") {
+			alert("Please input a number!");
+			return;
+		}
+
+		if (startNode < 0 || startNode > locations.length || finishNode < 0 || finishNode > locations.length) {
+			alert("Please input a valid node!");
+			return;
+		}
+
+
+		if (markers.length == 0) {
+			for (var i = 0; i < locations.length; i++) {
+				addMarker(locations[i]);
+			}
+		}
+
+		var steps = modes == 0 ? ucs(startNode, finishNode, distanceMatrix, locations) : aStar(startNode, finishNode, distanceMatrix, locations);
 
 		var length = steps.cost;
 		var path = steps.path;
 		var coords = [];
 
-		for (var i = 0; i < path.length; i++) {
-			coords.push(locations[path[i]]);
+		if (length != -1) {
+			document
+				.getElementById("length")
+				.innerHTML = "Distance: " + Math.floor(length * 1000).toString() + " m";
+	
+			for (var i = 0; i < path.length; i++) {
+				coords.push(locations[path[i]]);
+			}
+	
+			var pathString = path[0].toString();
+			for (var i = 1; i < path.length; i++) {
+				pathString += " → " + (path[i] + 1).toString();
+			}
+	
+			document
+				.getElementById("path-taken")
+				.innerHTML = pathString;
+	
+			drawPath(coords, "#FF0000", 2);
 		}
+		else {
+			document
+				.getElementById("length")
+				.innerHTML = "Length: not reachable";
 
-		drawPath(coords, "#FF0000");
+			document
+				.getElementById("path-taken")
+				.innerHTML = "Path not reachable";
+		}
 	}
 }
 
 // Main function
 async function initMap() {
-	// Ignore this, seems important
+	// Ignore this seems important
 	const { Map } = await google.maps.importLibrary("maps");
 
 	// Map options
@@ -143,7 +203,7 @@ async function initMap() {
 	map = new Map(document.getElementById("map"), options);
 
 	// Add marker when map is clicked
-	google.maps.event.addListener(map, "click", 
+	google.maps.event.addListener(map, "click",
 		function(event) {
 			var latVal = event.latLng.lat();
 			var lngVal = event.latLng.lng();
@@ -154,9 +214,9 @@ async function initMap() {
 		}
 	);
 
-	addMarker({ lat: -6.890327303831887, lng: 107.61034858365062 });
+	// addMarker({ lat: -6.890327303831887 107.61034858365062 });
 
-	locations.push({ lat: -6.890327303831887, lng: 107.61034858365062 });
+	// locations.push({ lat: -6.890327303831887 107.61034858365062 });
 }
 
 initMap();
@@ -165,26 +225,76 @@ initMap();
 var algorithm = document.getElementById("algorithm");
 var modes = 0;
 
-algorithm.addEventListener("click", function() {
-	document
-		.getElementById("ucs")
-		.addEventListener("click", function() {
-			algorithm.innerHTML = 'UCS';
-			modes = 1;
+// Other stuff not fit in function
+if (algorithm != null) {
+	algorithm.addEventListener("click", function() {
+		document
+			.getElementById("ucs")
+			.addEventListener("click", function() {
+				algorithm.innerHTML = 'Uniform Cost Search';
+				modes = 0;
+		});
+		
+		document
+			.getElementById("a-star")
+			.addEventListener("click", function() {
+				algorithm.innerHTML = 'A*';
+				modes = 1;
+		});
 	});
+
+	document
+		.getElementById("find-shortest")
+		.addEventListener("click", findShortestPath);
 	
+		document
+		.getElementById("clear")
+		.addEventListener("click", deleteMarker);
+
 	document
-		.getElementById("a-star")
-		.addEventListener("click", function() {
-			algorithm.innerHTML = 'A-Star';
-			modes = 2;
+		.getElementById("file-selector")
+		.addEventListener("change",
+			function(event) {
+				const file = event.target.files[0];
+
+				let reader = new FileReader();
+
+				reader.readAsText(file);
+				
+				reader.onload = function() {
+					var string = reader.result;
+
+					string = string.split('\n');
+	
+					var length = parseInt(string[0]);
+
+					locations = [];
+					for (var i = 0; i < length; i++) {
+						var loc = string[i + 1].split(' ');
+						var coord = {lat: parseFloat(loc[0]), lng: parseFloat(loc[1])};
+						
+						locations.push(coord);
+						addMarker(coord);
+					}
+				};
+			}
+		);
+}
+
+var theme = document.getElementById("bd-theme");
+
+if (theme != null) {
+	theme.addEventListener("click", function() {
+		var element = document.getElementsByTagName("html")[0]
+		var logo = document.getElementById("mode-logo")
+	
+		if (element.attributes["data-bs-theme"].value == "light") {
+			element.setAttribute("data-bs-theme", "dark");
+			logo.setAttribute("href", "#moon-stars-fill")
+		}
+		else {
+			element.setAttribute("data-bs-theme", "light");
+			logo.setAttribute("href", "#sun-fill")
+		}
 	});
-});
-
-document
-	.getElementById("find-shortest")
-	.addEventListener("click", findShortestPath);
-
-document
-	.getElementById("clear")
-	.addEventListener("click", removePaths);
+}
